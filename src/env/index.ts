2729d23031b240d2cd6a6913ff922707134a7e72
Jonathan <jonathan.riosousa@gmail.com>
2024-04-25 15:02:07 -0300

    feat: add new database config

--- src/env/index.ts
+++ src/env/index.ts
@@ -9,6 +9,7 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
   SERVER_PORT: z.coerce.number().default(3333),
 })
